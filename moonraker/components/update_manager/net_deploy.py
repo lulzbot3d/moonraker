63982868f5aff91845b4d0dca78e68cb8603f851
Carl Smith <csmith@lulzbot.com>
2026-02-26 11:54:49 -0600

    Set fallback for mainsail to lulzbot3d

--- moonraker/components/update_manager/net_deploy.py
+++ moonraker/components/update_manager/net_deploy.py
@@ -160,7 +160,7 @@ class NetDeploy(AppDeploy):
     async def _detect_fallback(self) -> bool:
         # Only used by "web" app types to fallback on the previous version info
         fallback_defs = {
-            "mainsail": "mainsail-crew",
+            "mainsail": "lulzbot3d",
             "fluidd": "fluidd-core"
         }
         for fname in ("manifest.json", "manifest.webmanifest"):
